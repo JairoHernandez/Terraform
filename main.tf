22db6339b6f11c0f0c62eaa38d2fd2b22b30bd96
Jairo Hernandez <jairomh@hotmail.com>
2020-09-25 16:14:36 -0500

    Finished ch2 Deploy a Single Web Server: input output variables

--- main.tf
+++ main.tf
@@ -2,9 +2,27 @@ provider "aws" {
     region = "us-east-2"
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
+output "public" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
+}
+
 resource "aws_instance" "example" {
     ami = "ami-07efac79022b86107"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id] # reference, mores specifically resource attribute reference
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, world" > index.html
+        nohup busybox httpd -f -p ${var.server_port} &
+        EOF
 
     tags = {
         Name = "terraform-example"
@@ -12,3 +30,13 @@ resource "aws_instance" "example" {
 
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
